10bb32f1993eae20fd0e9d8151308b407ceba626
gsongg1 <157450853+gsongg1@users.noreply.github.com>
2024-06-15 17:26:51 -0600

    backend done

--- mern-exercise-tracker/backend/routes/exercises.js
+++ mern-exercise-tracker/backend/routes/exercises.js
@@ -25,4 +25,33 @@ router.route('/add').post((req, res) => {
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    console.log('Received request to get exercise with ID:', req.params.id);
+    Exercise.findById(req.params.id) // find  by id
+      .then(exercise => res.json(exercise))  // return as json , else return error
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+router.route('/:id').delete((req, res) => {  // if its delete request then finds and deletes
+    Exercise.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Exercise deleted.')) 
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+router.route('/update/:id').post((req, res) => { //if route is update/ object id and is post, then we update it
+    Exercise.findById(req.params.id) // find current exercise and update
+      .then(exercise => {  
+        exercise.username = req.body.username;  // sets new exercise variables to equal the new data
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+  
+ exercise.save() // saves 
+    .then(() => res.json('Exercise updated!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+      })
+    .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+
 module.exports = router;
